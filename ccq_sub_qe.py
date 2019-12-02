ac44c995bcd9deb66c71862a54e6b467a4a87e2c
Brian Busemeyer <bbusemeyer@gmail.com>
2019-12-02 17:22:27 -0500

    modified:   ccq_sub_qe.py 	modified:   ccqlin023_aliases

--- ccq_sub_qe.py
+++ ccq_sub_qe.py
@@ -21,13 +21,15 @@ def main():
       help='Run locally.'+dft)
   parser.add_argument('-nk',dest='nk',default=1,type=int,
       help='Number of QE "pools" corresponding to division of kpoints.'+dft)
+  parser.add_argument('-ni',dest='ni',default=1,type=int,
+      help='Number of QE "images" corresponding to division of phonon qpoints.'+dft)
   parser.add_argument('--ptype',dest='ptype',default=None,
       help='Processor type to enforce (skylake or broadwell).'+dft)
   args=parser.parse_args()
 
-  qsub(exe=args.exe,time=args.time,inpfn=args.inpfn,queue=args.queue,local=args.local,nn=args.nn,nk=args.nk,ptype=args.ptype)
+  qsub(exe=args.exe,time=args.time,inpfn=args.inpfn,queue=args.queue,local=args.local,nn=args.nn,nk=args.nk,ni=args.ni,ptype=args.ptype)
 
-def qsub(inpfn,exe='pw',local=False,nn=1,time='1:00:00',queue='ccq',nk=1,ptype=None):
+def qsub(inpfn,exe='pw',local=False,nn=1,time='1:00:00',queue='ccq',nk=1,ni=1,ptype=None):
   ptypeline = [f"#SBATCH -C {ptype}"] if ptype is not None else []
   outlines = [
       f"#!/bin/bash",
@@ -41,8 +43,8 @@ def qsub(inpfn,exe='pw',local=False,nn=1,time='1:00:00',queue='ccq',nk=1,ptype=N
     ] + ptypeline + [
       f"cd %s"%os.getcwd(),
       f"module purge",
-      f"module load slurm gcc lib/fftw3 lib/openblas openmpi intel/mkl",
-      f"mpirun /mnt/home/bbusemeyer/bin/{exe}.x -input {inpfn} -nk {nk}"
+      f"module load slurm gcc openmpi2 lib/fftw3/3.3.8-openmpi2 intel/mkl",
+      f"mpirun /mnt/home/bbusemeyer/bin/{exe}.x -input {inpfn} -nk {nk} -ni {ni}"
     ]
 
   with open('qsub.in','w') as outf:
